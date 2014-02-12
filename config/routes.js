26910fe516e95912c2fdcafbcafe48bf583d92ff
Andy Jiang <andyjiang@gmail.com>
2014-02-12 15:17:06 -0800

    added fake data in the server

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,72 @@
 'use strict';
 
+// var moment = require('./moment');
+
 (function() {
+
+var events = [
+		{
+			title: 'Dinner at State Bird Provisions',
+			neighborhood: 'Western Addition',
+			datetime: Date.now,
+			partyMax: 12,
+			attendees: [
+				{
+					name: '',
+					imgUrl: ''
+				},
+				{
+					name: '',
+					imgUrl: ''
+				}
+			],
+			pictures: [
+				{
+					imgUrl: '',
+					altTxt: ''
+				},
+				{
+					imgUrl: '',
+					altTxt: ''
+				}
+			]
+		},
+		{
+			title: 'Brunch at Serpentine',
+			neighborhood: 'Dogpatch',
+			datetime: Date.now,
+			partyMax: 4,
+			attendees: [
+				{
+					name: '',
+					imgUrl: ''
+				},
+				{
+					name: '',
+					imgUrl: ''
+				}
+			],
+			pictures: [
+				{
+					imgUrl: '',
+					altTxt: ''
+				},
+				{
+					imgUrl: '',
+					altTxt: ''
+				}
+			]
+		}
+	];
 
 // Public functions. ===========================================================
 module.exports = function (app) {
+	// API endpoints. ============================================================
+	app.get('/api/events', function (req, res) {
+		// Return events.
+		res.send(events, 200);
+	});
+
 	// Application route =========================================================
 	app.get('/*', function (req, res) {
     res.sendfile('index.html', {'root': './public/views/'});
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,8 +1,26 @@
 'use strict';
 
-weeblyApp.controller('mainController',
+nommeryApp.controller('mainController',
   ['$scope', '$http',
 	function ($scope, $http)
 {
+	// Initialize variables.
+	var events = $scope.events = [];
 
+	// Events.
+	// - restaurantName
+	// - restaurantNeighborhood
+	// - Pictures of food
+	// - Rating
+	// Time date
+
+	// Load available events.
+	$http.get('/api/events')
+	.success( function (data) {
+		events = data;
+		console.log('Event data: ' + data);
+	})
+	.error( function (data) {
+		console.log('Server error: ' + data);
+	});
 }]);
